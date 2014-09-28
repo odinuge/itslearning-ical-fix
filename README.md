e441dcf0c642ec5ce203cabd61d39435d7ea42ad
Odin Ugedal <odin@ugedal.com>
2014-09-28 15:03:10 +0200

    Add initial code

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 UgeSyspanel
 ======================
-Fixingng itslearning iCalendars, one event at a time.
+Fixing itslearning iCalendars, one event at a time.
+The itslearning iCalendars are made with a event SUMMARY
+containing the name of the calendar, and not the title.
+This make all events look the same, and therefore the calendar
+is useless.
 
-#In works!
+This simple webserver fixes the provided calendar, and returns
+a new and way better one!
+
+Usage
+======================
+Start server:
+     python itslearningiCalFix.py
+
+API for Fixing calendar:
+     http://host:8888/icalendar.ical?url=yoururl
+
+Replace "yoururl" with the url to the ical calendar.
+
+PS: To make it work; remember to encode the url.
+
 
 Requirements
 ======================
-Yes
+[Python](https://www.python.org/), [Tornado](https://pypi.python.org/pypi/tornado) and [iCalendar](https://pypi.python.org/pypi/icalendar)
 
 License
 ======================
-See LICENSE.md		
+See LICENSE.md
---
+++ itslearningiCalFix.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import urllib.request
+
+import tornado.ioloop
+import tornado.web
+import tornado.options
+
+from urllib.request import HTTPError, URLError
+
+from tornado.escape import json_encode
+
+from icalendar import Calendar
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        """The http GET function"""
+        if self.request.uri == "/":
+            self.redirect("/calendar.ical")
+            return
+        
+        calendarURL = self.get_argument("url", None, True)
+        if calendarURL == None:
+            self.write("No url provided")
+            return
+
+        # Try to download the provided url
+        try:
+            ical = urllib.request.urlopen(calendarURL.replace("webcal", "http")).read()
+        except ValueError as err:
+            self.returnError(str(err))
+            return
+
+        except HTTPError as err:
+            self.returnError(str(err), err.getcode())
+            return
+
+        except URLError as err:
+            self.returnError(str(err))
+            return
+
+        # Try to parse the ical
+        try:
+            cal = Calendar.from_ical(ical)
+        except ValueError as err:
+            self.returnError("Not a valid iCalendar file")
+            return
+
+        for item in cal.walk('vevent'):
+            # Switch SUMMARY and DESCRIPTION to make the calendar usable
+            summary = item['SUMMARY']
+            item['SUMMARY'] = item['DESCRIPTION']
+            item['DESCRIPTION'] = item['DESCRIPTION'] + "\n" + summary
+            cal_new.add_component(item)
+
+        # Return the "New" iCalendar file
+        self.set_header("Content-Type", 'text/calendar; charset="utf-8"')
+        self.write(cal.to_ical())
+
+    def returnError(self, err_msg, err_code=400):
+        """Return error in json format for easy debugging"""
+        res = {
+            'success' : 'false',
+            'error' : err_msg,
+        }
+        self.set_status(err_code)
+        self.write(json_encode(res)+ "\n")
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/calendar.ical", MainHandler),
+])
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
